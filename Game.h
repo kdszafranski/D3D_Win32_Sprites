13a5bb3c4c620cdb68278badddfe630990db20aa
Kris Szafranski <kris@thoughtshelter.com>
2023-10-24 16:08:47 -0500

    Add project files.

---
+++ Game.h
@@ -0,0 +1,73 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+typedef DirectX::SimpleMath::Vector2 vector2;
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop.
+class Game final : public DX::IDeviceNotify
+{
+public:
+
+    Game() noexcept(false);
+    ~Game() = default;
+
+    Game(Game&&) = default;
+    Game& operator= (Game&&) = default;
+
+    Game(Game const&) = delete;
+    Game& operator= (Game const&) = delete;
+
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+
+    // IDeviceNotify
+    void OnDeviceLost() override;
+    void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowMoved();
+    void OnDisplayChange();
+    void OnWindowSizeChanged(int width, int height);
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const noexcept;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+    void Render();
+
+    void Clear();
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                           m_timer;
+
+    // our lovely cat image
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_texture;
+    std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
+    // position to draw the image
+    vector2 m_screenPos;
+    vector2 m_origin;
+    // dealing with alpha blending
+    std::unique_ptr<DirectX::CommonStates> m_states;
+};
